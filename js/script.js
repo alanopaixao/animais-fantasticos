199056f578734874acfcf918973242a36b0d542b
Alan Paixao <alanpaixao2@gmail.com>
2023-11-10 18:35:34 -0300

    fetch-bitcoin

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
 import initfetchAnimais from './modules/fetch-animais.js.js';
+import initFetchBitcoin from './modules/fetch-bitcoin.js';
 
 initScrollSuave();
 initAnimacaoScroll();
@@ -19,3 +20,4 @@ initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
 initfetchAnimais();
+initFetchBitcoin();
